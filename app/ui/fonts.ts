08ad5d1fa333ccaeb864d20a2ebe6dbd98e58c7c
VardanHeroic <tarayanvardan@gmail.com>
2025-08-09 06:27:05 +0400

    added subsets for mono font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,9 @@
 import { Inter, Source_Code_Pro, Lusitana } from "next/font/google"
 
 export const inter = Inter({ subsets: ["latin"] })
-export const mono = Source_Code_Pro()
+export const mono = Source_Code_Pro({
+	subsets: ["latin"],
+})
 export const lusitana = Lusitana({
 	weight: ["400", "700"],
 	subsets: ["latin"],
