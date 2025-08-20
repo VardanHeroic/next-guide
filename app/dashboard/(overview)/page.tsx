57c4966c903330df4ee0802a9e48a6a3d72a7caf
VardanHeroic <tarayanvardan@gmail.com>
2025-08-20 06:47:47 +0400

    chapter 16

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,7 +6,10 @@ import { fetchCardData } from "@/app/lib/data"
 import { Suspense } from "react"
 import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from "@/app/ui/skeletons"
 import CardWrapper from "@/app/ui/dashboard/cards"
-
+import { Metadata } from "next"
+export const metadata: Metadata = {
+	title: "Dashboard",
+}
 export default async function Page() {
 	return (
 		<main>
